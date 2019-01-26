734b348e1ae66b501d2c149e9a5841d79f85a552
Marc Kohaupt <debuglevel@gmail.com>
2019-01-26 23:01:12 +0100

    Use de.debuglevel.odbjdbc library

--- src/main/kotlin/de/debuglevel/cvodb2xml/import/odb/OdbImporter.kt
+++ src/main/kotlin/de/debuglevel/cvodb2xml/import/odb/OdbImporter.kt
@@ -1,38 +1,81 @@
 package de.debuglevel.cvodb2xml.import.odb
 
-import de.debuglevel.cvodb2xml.UnzipUtility
+import com.github.rjeschke.txtmark.Processor
 import de.debuglevel.cvodb2xml.import.Importer
-import de.debuglevel.cvodb2xml.import.hsqldb.HsqldbImporter
 import de.debuglevel.cvodb2xml.model.Position
 import mu.KotlinLogging
-import java.nio.file.Files
 import java.nio.file.Path
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+import java.time.LocalDate
 
-class OdbImporter(odbPath: Path) : Importer {
+class OdbImporter(private val odbPath: Path) : Importer {
     private val logger = KotlinLogging.logger {}
-
-    private val hsqldbImporter: HsqldbImporter
 
     init {
         logger.debug { "Initializing OdbImporter..." }
 
-        logger.debug { "Creating temporary directory for ODB extraction..." }
-        val odbExtractedPath = createTempDir()
-        logger.debug { "Created temporary directory for ODB extraction: ${odbExtractedPath.absolutePath}" }
-
-        logger.debug { "Unzipping ODB '${odbPath.toAbsolutePath()}' to '${odbExtractedPath.absolutePath}'..." }
-        UnzipUtility().unzip(odbPath.toAbsolutePath().toString(), odbExtractedPath.absolutePath)
-        logger.debug { "Unzipped ODB '${odbPath.toAbsolutePath()}' to '${odbExtractedPath.absolutePath}'" }
-
-        val databasePath = odbExtractedPath.resolve("database")
-        databasePath.listFiles().forEach {
-            logger.debug { "Renaming ${it.toPath()} to ${it.toPath().resolveSibling("db.${it.name}")}..." }
-            Files.move(it.toPath(), it.toPath().resolveSibling("database.${it.name}"))
-        }
-
-        hsqldbImporter = HsqldbImporter(databasePath.toPath())
+        logger.debug { "Loading OdbDriver..." }
+        Class.forName("de.debuglevel.odbjdbc.OdbDriver")
     }
 
     override val positions: List<Position>
-        get() = hsqldbImporter.positions
+        get() {
+            val databasePath = odbPath.toFile().invariantSeparatorsPath
+
+            var connection: Connection? = null
+            val database = "jdbc:odb://file=$databasePath"
+
+            val positions = mutableListOf<Position>()
+
+            try {
+                // Create database connection
+                logger.debug { "Opening connection to $database..." }
+                connection = DriverManager.getConnection(database)
+
+                // Create and execute statement
+                val statement = connection.createStatement()
+                val resultset = statement.executeQuery("SELECT * FROM \"PUBLIC\".\"Positionen\"")
+
+                while (resultset.next()) {
+                    logger.debug { "Reading next position from database..." }
+                    positions.add(
+                        Position(
+                            resultset.getString("id").toInt(),
+                            resultset.getString("positiontitle"),
+                            Processor.process(resultset.getString("description")),
+                            LocalDate.parse(resultset.getString("begindate")),
+                            if (resultset.getString("enddate").isNullOrBlank()) null else LocalDate.parse(
+                                resultset.getString(
+                                    "enddate"
+                                )
+                            ),
+                            resultset.getString("place"),
+                            resultset.getString("placeurl"),
+                            resultset.getString("category"),
+                            resultset.getString("industrialsector"),
+                            resultset.getString("placesize"),
+                            resultset.getString("department"),
+                            resultset.getString("activity")
+                        )
+                    )
+                }
+
+                resultset.close()
+                statement.close()
+            } catch (e: SQLException) {
+                logger.error(e) { }
+                throw e
+            } finally {
+                try {
+                    connection?.close()
+                } catch (e: SQLException) {
+                    logger.error(e) { }
+                    throw e
+                }
+            }
+
+            return positions
+        }
 }
